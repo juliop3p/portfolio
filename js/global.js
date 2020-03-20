c7182efba7a7c8caabe181383cc20965e6553339
Julio Cesar <julio15.zn@gmail.com>
2020-03-20 16:04:16 -0300

    nav current page effect updated

--- js/global.js
+++ js/global.js
@@ -3,7 +3,7 @@ document.querySelector('#navSmallScreen').onclick = () => {
     document.querySelector('.nav-small').classList.toggle('show')
 }
 
-const page = document.URL.split('/')[4].split('.')[0]
+const page = document.URL.split('/')[3].split('.')[0]
 
 document.querySelectorAll('.list-item').forEach(elem => {
 
